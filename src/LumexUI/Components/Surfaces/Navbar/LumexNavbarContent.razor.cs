7759c321e5f13918e8df8d769c8f04d084f6b6c5
desmondinho <>
2024-02-24 15:51:17 +0200

    Components(navbar): Update LumexNavbarContent markup and styles

--- src/LumexUI/Components/Surfaces/Navbar/LumexNavbarContent.razor.cs
+++ src/LumexUI/Components/Surfaces/Navbar/LumexNavbarContent.razor.cs
@@ -2,7 +2,6 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
-using LumexUI.Common;
 using LumexUI.Utilities;
 
 using Microsoft.AspNetCore.Components;
@@ -11,11 +10,10 @@ namespace LumexUI;
 
 public partial class LumexNavbarContent : LumexNavBase
 {
-	[CascadingParameter] private LumexNavbar Parent { get; set; } = default!;
+	[CascadingParameter] internal LumexNavbar Parent { get; set; } = default!;
 
 	protected override string RootClass =>
 		new CssBuilder( $"{Parent.Name}-content" )
-			.AddClass( $"{Parent.Name}--vertical", when: Orientation is Orientation.Vertical )
 			.AddClass( $"{Parent.Slots.Content}", when: !string.IsNullOrEmpty( Parent.Slots.Content ) )
 			.AddClass( base.RootClass )
 		.Build();
