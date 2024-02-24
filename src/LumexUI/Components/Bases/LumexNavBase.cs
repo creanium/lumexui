d3456f5ff88a7110271f2225ee84b912c22ba142
desmondinho <daniil.norinn@gmail.com>
2024-02-24 15:17:51 +0200

    Components(nav): Rename LumexNavMenu to LumexNav

--- src/LumexUI/Components/Bases/LumexNavBase.cs
+++ src/LumexUI/Components/Bases/LumexNavBase.cs
@@ -2,10 +2,7 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
-using LumexUI.Common;
-
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
 
 namespace LumexUI;
 
@@ -15,20 +12,4 @@ public abstract class LumexNavBase : LumexComponentBase
 	/// Defines the content to be rendered inside the navigation.
 	/// </summary>
 	[Parameter] public RenderFragment? ChildContent { get; set; }
-
-	/// <summary>
-	/// Defines the orientation of the navigation.
-	/// </summary>
-	/// <remarks>Default is <see cref="Orientation.Horizontal"/></remarks>
-	[Parameter] public Orientation Orientation { get; set; }
-
-	protected override void BuildRenderTree( RenderTreeBuilder builder )
-	{
-		builder.OpenElement( 0, "ul" );
-		builder.AddAttribute( 1, "class", RootClass );
-		builder.AddAttribute( 2, "style", RootStyle );
-		builder.AddMultipleAttributes( 3, AdditionalAttributes );
-		builder.AddContent( 4, ChildContent );
-		builder.CloseElement();
-	}
 }
---
+++ src/LumexUI/Components/Navigation/Menu/LumexNav.razor.cs
@@ -0,0 +1,31 @@
+// Copyright (c) LumexUI 2024
+// LumexUI licenses this file to you under the MIT license
+// See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
+
+using LumexUI.Common;
+using LumexUI.Utilities;
+
+using Microsoft.AspNetCore.Components;
+
+namespace LumexUI;
+
+public partial class LumexNav : LumexNavBase, ISlotComponent<NavSlots>
+{
+	/// <summary>
+	/// Defines the orientation of the navigation.
+	/// </summary>
+	/// <remarks>Default is <see cref="Orientation.Horizontal"/></remarks>
+	[Parameter] public Orientation Orientation { get; set; }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	[Parameter] public NavSlots Slots { get; set; } = new();
+
+	protected override string RootClass =>
+		new CssBuilder( $"{Name}-root" )
+			.AddClass( $"{Name}--vertical", when: Orientation is Orientation.Vertical )
+			.AddClass( $"{Slots.Root}", when: !string.IsNullOrEmpty( Slots.Root ) )
+			.AddClass( base.RootClass )
+		.Build();
+}
