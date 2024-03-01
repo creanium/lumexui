397ef721d11e6c5b8653ff75fb847afc328e5d7e
desmondinho <daniil.norinn@gmail.com>
2024-03-01 23:00:47 +0200

    Components(theme): Update typography settings

--- src/LumexUI/Theme/Typography.cs
+++ src/LumexUI/Theme/Typography.cs
@@ -6,17 +6,37 @@ namespace LumexUI.Theme;
 
 public record Typography
 {
-	public string? SansSerif { get; init; }
+    public FontFamilies FontFamilies { get; init; } = new();
 
-	public string? Monospace { get; init; }
+	public FontSizes FontSizes { get; init; } = new();
 
-	public string FontSize { get; init; } = "1rem";
+	public LineHeights LineHeights { get; init; } = new();
 
-	public string FontWeight { get; init; } = "400";
+	public string? FontWeight { get; init; } = "400";
 
-	public string LineHeight { get; init; } = "1.5";
+	internal static string DefaultSansSerif => "system-ui,-apple-system,Segoe UI,Roboto,Helvetica Neue,Noto Sans,Liberation Sans,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;";
 
-	internal string DefaultSansSerif => "system-ui,-apple-system,Segoe UI,Roboto,Helvetica Neue,Noto Sans,Liberation Sans,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;";
+	internal static string DefaultMonospace => "SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace";
+}
 
-	internal string DefaultMonospace => "SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace";
+public record FontFamilies
+{
+    public string? SansSerif { get; init; }
+    public string? Monospace { get; init; }
+}
+
+public record FontSizes
+{
+	public string? Xs { get; init; } = ".75rem";
+    public string? Sm { get; init; } = ".875rem";
+    public string? Md { get; init; } = "1rem";
+    public string? Lg { get; init; } = "1.25rem";
+}
+
+public record LineHeights
+{
+    public string? Xs { get; init; } = "1rem";
+    public string? Sm { get; init; } = "1.25rem";
+    public string? Md { get; init; } = "1.5rem";
+    public string? Lg { get; init; } = "1.75rem";
 }
