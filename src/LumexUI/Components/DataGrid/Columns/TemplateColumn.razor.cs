2e0f9751941addcb99a505d03f8ae31bde7fb9cf
desmondinho <daniil.norinn@gmail.com>
2024-11-05 01:49:57 +0200

    feat: add template column

---
+++ src/LumexUI/Components/DataGrid/Columns/TemplateColumn.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LumexUI;
+
+/// <summary>
+/// A component representing a <see cref="LumexDataGrid{T}"/> column whose cells render a supplied template.
+/// </summary>
+/// <typeparam name="T">The type of data represented by each row in the grid.</typeparam>
+public partial class TemplateColumn<T> : LumexColumnBase<T>
+{
+    private static readonly RenderFragment<T> EmptyChildContent = _ => builder => { };
+
+    /// <summary>
+    /// Gets or sets the content to be rendered for each row in the table.
+    /// </summary>
+    [Parameter] public RenderFragment<T> ChildContent { get; set; } = EmptyChildContent;
+}
