29bd2e110c4b960ce99c931fa3cf0a141a041089
desmondinho <68395709+desmondinho@users.noreply.github.com>
2025-01-30 03:42:34 +0200

    build: add compatibility for .NET 9 (#161)
    * build: add compatibility for .NET 9
    
    * ci(build-test): fix yaml
    
    * ci(deploy): fix yaml
    
    * ci(nuget): fix yaml
    
    * fix(tailwind): typo

--- docs/LumexUI.Docs/tailwind.config.js
+++ docs/LumexUI.Docs/tailwind.config.js
@@ -1,5 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const lumexui = '../../src/LumexUI/';
+const lumexui = '../../src/LumexUI';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
