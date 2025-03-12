8d21401830f61718d783285e00f5bd2dd85e5559
desmondinho <68395709+desmondinho@users.noreply.github.com>
2025-03-12 14:45:01 +0200

    fix(theme): remove extra shade (950) from the color scales for consistency in dark mode (#199)
    * fix(theme): remove extra key (950) from the color scales for consistency in dark mode
    
    * build(docs): explicitly set Tailwind v4.0.9

---
+++ src/LumexUI/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LumexUI.Extensions;
+
+[ExcludeFromCodeCoverage]
+internal static class DictionaryExtensions
+{
+	public static Dictionary<TKey, TValue> TakeAllExceptLast<TKey, TValue>( this IReadOnlyDictionary<TKey, TValue> source, int count = 1 )
+		where TKey : notnull
+	{
+		ArgumentNullException.ThrowIfNull( source, nameof( source ) );
+
+		if( count < 1 )
+		{
+			throw new ArgumentOutOfRangeException( nameof( count ), count, "The count must be greater than one." );
+		}
+
+		if( count >= source.Count )
+		{
+			return [];
+		}
+
+		return source
+			.Take( source.Count - count )
+			.ToDictionary( x => x.Key, x => x.Value );
+	}
+}
