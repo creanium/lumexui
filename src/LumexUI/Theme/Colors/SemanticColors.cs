2d22ffc3d021f897ea813d952ac1671e22a91755
desmondinho <68395709+desmondinho@users.noreply.github.com>
2025-03-12 15:03:20 +0200

    fix(theme): remove extra shade (950) from the color scales for consistency in dark mode (#199) (#200)
    * fix(theme): remove extra key (950) from the color scales for consistency in dark mode
    
    * build(docs): explicitly set Tailwind v4.0.9

--- src/LumexUI/Theme/Colors/SemanticColors.cs
+++ src/LumexUI/Theme/Colors/SemanticColors.cs
@@ -2,6 +2,8 @@
 // LumexUI licenses this file to you under the MIT license
 // See the license here https://github.com/LumexUI/lumexui/blob/main/LICENSE
 
+using LumexUI.Extensions;
+
 using static LumexUI.Utilities.ColorUtils;
 
 namespace LumexUI.Theme;
@@ -11,146 +13,146 @@ namespace LumexUI.Theme;
 /// </summary>
 public static class SemanticColors
 {
-    private const string Default = "default";
-    private const string Foreground = "foreground";
+	private const string Default = "default";
+	private const string Foreground = "foreground";
 
-    /// <summary>
-    /// Represents the theme colors for the light theme.
-    /// </summary>
-    public readonly static ThemeColors Light = new()
-    {
-        Background = [
-            new( Default, Colors.White )
-        ],
-        Foreground = [
-            .. Colors.Zinc,
-            new( Default, Colors.Zinc["700"] ),
-        ],
-        Overlay = [
-            new( Default, Colors.Black )
-        ],
-        Focus = [
-            new( Default, Colors.Blue["500"] )
-        ],
-        Divider = [
-            new( Default, Colors.Zinc["900"] )
-        ],
-        Surface1 = [
-            new( Default, Colors.White ),
-            new( Foreground, Colors.Zinc["900"] )
-        ],
-        Surface2 = [
-            new( Default, Colors.Zinc["100"] ),
-            new( Foreground, Colors.Zinc["800"] )
-        ],
-        Surface3 = [
-            new( Default, Colors.Zinc["200"] ),
-            new( Foreground, Colors.Zinc["700"] )
-        ],
-        Default = [
-            .. Colors.Zinc,
-            new( Default, Colors.Zinc["300"] ),
-            new( Foreground, GetReadableColor( Colors.Zinc["300"] ) )
-        ],
-        Primary = [
-            .. Colors.Blue,
-            new( Default, Colors.Blue["500"] ),
-            new( Foreground, GetReadableColor( Colors.Blue["500"] ) )
-        ],
-        Secondary = [
-            .. Colors.Violet,
-            new( Default, Colors.Violet["500"] ),
-            new( Foreground, GetReadableColor( Colors.Violet["500"] ) )
-        ],
-        Success = [
-            .. Colors.Green,
-            new( Default, Colors.Green["500"] ),
-            new( Foreground, GetReadableColor( Colors.Green["500"] ) )
-        ],
-        Warning = [
-            .. Colors.Amber,
-            new( Default, Colors.Amber["500"] ),
-            new( Foreground, GetReadableColor( Colors.Amber["500"] ) )
-        ],
-        Danger = [
-            .. Colors.Rose,
-            new( Default, Colors.Rose["500"] ),
-            new( Foreground, GetReadableColor( Colors.Rose["500"] ) )
-        ],
-        Info = [
-            .. Colors.Sky,
-            new( Default, Colors.Sky["500"] ),
-            new( Foreground, GetReadableColor( Colors.Sky["500"] ) )
-        ]
-    };
+	/// <summary>
+	/// Represents the theme colors for the light theme.
+	/// </summary>
+	public readonly static ThemeColors Light = new()
+	{
+		Background = [
+			new( Default, Colors.White )
+		],
+		Foreground = [
+			.. Colors.Zinc.TakeAllExceptLast(),
+			new( Default, Colors.Zinc["700"] ),
+		],
+		Overlay = [
+			new( Default, Colors.Black )
+		],
+		Focus = [
+			new( Default, Colors.Blue["500"] )
+		],
+		Divider = [
+			new( Default, Colors.Zinc["900"] )
+		],
+		Surface1 = [
+			new( Default, Colors.White ),
+			new( Foreground, Colors.Zinc["900"] )
+		],
+		Surface2 = [
+			new( Default, Colors.Zinc["100"] ),
+			new( Foreground, Colors.Zinc["800"] )
+		],
+		Surface3 = [
+			new( Default, Colors.Zinc["200"] ),
+			new( Foreground, Colors.Zinc["700"] )
+		],
+		Default = [
+			.. Colors.Zinc.TakeAllExceptLast(),
+			new( Default, Colors.Zinc["300"] ),
+			new( Foreground, GetReadableColor( Colors.Zinc["300"] ) )
+		],
+		Primary = [
+			.. Colors.Blue.TakeAllExceptLast(),
+			new( Default, Colors.Blue["500"] ),
+			new( Foreground, GetReadableColor( Colors.Blue["500"] ) )
+		],
+		Secondary = [
+			.. Colors.Violet.TakeAllExceptLast(),
+			new( Default, Colors.Violet["500"] ),
+			new( Foreground, GetReadableColor( Colors.Violet["500"] ) )
+		],
+		Success = [
+			.. Colors.Green.TakeAllExceptLast(),
+			new( Default, Colors.Green["500"] ),
+			new( Foreground, GetReadableColor( Colors.Green["500"] ) )
+		],
+		Warning = [
+			.. Colors.Amber.TakeAllExceptLast(),
+			new( Default, Colors.Amber["500"] ),
+			new( Foreground, GetReadableColor( Colors.Amber["500"] ) )
+		],
+		Danger = [
+			.. Colors.Rose.TakeAllExceptLast(),
+			new( Default, Colors.Rose["500"] ),
+			new( Foreground, GetReadableColor( Colors.Rose["500"] ) )
+		],
+		Info = [
+			.. Colors.Sky.TakeAllExceptLast(),
+			new( Default, Colors.Sky["500"] ),
+			new( Foreground, GetReadableColor( Colors.Sky["500"] ) )
+		]
+	};
 
-    /// <summary>
-    /// Represents the theme colors for the dark theme.
-    /// </summary>
-    public readonly static ThemeColors Dark = new()
-    {
-        Background = [
-            new( Default, Colors.Black )
-        ],
-        Foreground = [
-            .. Colors.ReverseColorValues( Colors.Zinc ),
-            new( Default, Colors.Zinc["200"] ),
-        ],                              
-        Overlay = [
-            new( Default, Colors.Black )
-        ],
-        Focus = [
-            new( Default, Colors.Blue["500"] )
-        ],
-        Divider = [
-            new( Default, Colors.Zinc["50"] )
-        ],
-        Surface1 = [
-            new( Default, Colors.Zinc["900"] ),
-            new( Foreground, Colors.Zinc["50"] )
-        ],
-        Surface2 = [
-            new( Default, Colors.Zinc["800"] ),
-            new( Foreground, Colors.Zinc["100"] )
-        ],
-        Surface3 = [
-            new( Default, Colors.Zinc["700"] ),
-            new( Foreground, Colors.Zinc["200"] )
-        ],
-        Default = [
-            .. Colors.ReverseColorValues( Colors.Zinc ),
-            new( Default, Colors.Zinc["300"] ),
-            new( Foreground, GetReadableColor( Colors.Zinc["300"] ) )
-        ],
-        Primary = [
-            .. Colors.ReverseColorValues( Colors.Blue ),
-            new( Default, Colors.Blue["500"] ),
-            new( Foreground, GetReadableColor( Colors.Blue["500"] ) )
-        ],
-        Secondary = [
-            .. Colors.ReverseColorValues( Colors.Violet ),
-            new( Default, Colors.Violet["500"] ),
-            new( Foreground, GetReadableColor( Colors.Violet["500"] ) )
-        ],
-        Success = [
-            .. Colors.ReverseColorValues( Colors.Green ),
-            new( Default, Colors.Green["500"] ),
-            new( Foreground, GetReadableColor( Colors.Green["500"] ) )
-        ],
-        Warning = [
-            .. Colors.ReverseColorValues( Colors.Amber ),
-            new( Default, Colors.Amber["500"] ),
-            new( Foreground, GetReadableColor( Colors.Amber["500"] ) )
-        ],
-        Danger = [
-            .. Colors.ReverseColorValues( Colors.Rose ),
-            new( Default, Colors.Rose["500"] ),
-            new( Foreground, GetReadableColor( Colors.Rose["500"] ) )
-        ],
-        Info = [
-            .. Colors.ReverseColorValues( Colors.Sky ),
-            new( Default, Colors.Sky["500"] ),
-            new( Foreground, GetReadableColor( Colors.Sky["500"] ) )
-        ]
-    };
+	/// <summary>
+	/// Represents the theme colors for the dark theme.
+	/// </summary>
+	public readonly static ThemeColors Dark = new()
+	{
+		Background = [
+			new( Default, Colors.Black )
+		],
+		Foreground = [
+			.. Colors.ReverseColorValues( Colors.Zinc.TakeAllExceptLast() ),
+			new( Default, Colors.Zinc["200"] ),
+		],
+		Overlay = [
+			new( Default, Colors.Black )
+		],
+		Focus = [
+			new( Default, Colors.Blue["500"] )
+		],
+		Divider = [
+			new( Default, Colors.Zinc["50"] )
+		],
+		Surface1 = [
+			new( Default, Colors.Zinc["900"] ),
+			new( Foreground, Colors.Zinc["50"] )
+		],
+		Surface2 = [
+			new( Default, Colors.Zinc["800"] ),
+			new( Foreground, Colors.Zinc["100"] )
+		],
+		Surface3 = [
+			new( Default, Colors.Zinc["700"] ),
+			new( Foreground, Colors.Zinc["200"] )
+		],
+		Default = [
+			.. Colors.ReverseColorValues( Colors.Zinc.TakeAllExceptLast() ),
+			new( Default, Colors.Zinc["300"] ),
+			new( Foreground, GetReadableColor( Colors.Zinc["300"] ) )
+		],
+		Primary = [
+			.. Colors.ReverseColorValues( Colors.Blue.TakeAllExceptLast() ),
+			new( Default, Colors.Blue["500"] ),
+			new( Foreground, GetReadableColor( Colors.Blue["500"] ) )
+		],
+		Secondary = [
+			.. Colors.ReverseColorValues( Colors.Violet.TakeAllExceptLast() ),
+			new( Default, Colors.Violet["500"] ),
+			new( Foreground, GetReadableColor( Colors.Violet["500"] ) )
+		],
+		Success = [
+			.. Colors.ReverseColorValues( Colors.Green.TakeAllExceptLast() ),
+			new( Default, Colors.Green["500"] ),
+			new( Foreground, GetReadableColor( Colors.Green["500"] ) )
+		],
+		Warning = [
+			.. Colors.ReverseColorValues( Colors.Amber.TakeAllExceptLast() ),
+			new( Default, Colors.Amber["500"] ),
+			new( Foreground, GetReadableColor( Colors.Amber["500"] ) )
+		],
+		Danger = [
+			.. Colors.ReverseColorValues( Colors.Rose.TakeAllExceptLast() ),
+			new( Default, Colors.Rose["500"] ),
+			new( Foreground, GetReadableColor( Colors.Rose["500"] ) )
+		],
+		Info = [
+			.. Colors.ReverseColorValues( Colors.Sky.TakeAllExceptLast() ),
+			new( Default, Colors.Sky["500"] ),
+			new( Foreground, GetReadableColor( Colors.Sky["500"] ) )
+		]
+	};
 }
--- tests/LumexUI.Tests/Theme/ThemeTests.cs
+++ tests/LumexUI.Tests/Theme/ThemeTests.cs
@@ -17,7 +17,7 @@ public class ThemeTests
         theme.Light.Colors.Background.Should().HaveCount( 1 );
         theme.Light.Colors.Background.Should().ContainKey( "default" );
 
-        theme.Light.Colors.Foreground.Should().HaveCount( 12 );
+        theme.Light.Colors.Foreground.Should().HaveCount( 11 );
         theme.Light.Colors.Foreground.Should().ContainKey( "default" );
         theme.Light.Colors.Foreground.Should().NotContainKey( "foreground" );
 
@@ -33,7 +33,7 @@ public class ThemeTests
         theme.Dark.Colors.Background.Should().HaveCount( 1 );
         theme.Dark.Colors.Background.Should().ContainKey( "default" );
 
-        theme.Dark.Colors.Foreground.Should().HaveCount( 12 );
+        theme.Dark.Colors.Foreground.Should().HaveCount( 11 );
         theme.Dark.Colors.Foreground.Should().ContainKey( "default" );
         theme.Dark.Colors.Foreground.Should().NotContainKey( "foreground" );
 
@@ -52,59 +52,59 @@ public class ThemeTests
     {
         var theme = new LumexTheme();
 
-        theme.Light.Colors.Default.Should().HaveCount( 13 );
+        theme.Light.Colors.Default.Should().HaveCount( 12 );
         theme.Light.Colors.Default.Should().ContainKey( "default" );
         theme.Light.Colors.Default.Should().ContainKey( "foreground" );
 
-        theme.Light.Colors.Primary.Should().HaveCount( 13 );
+        theme.Light.Colors.Primary.Should().HaveCount( 12 );
         theme.Light.Colors.Primary.Should().ContainKey( "default" );
         theme.Light.Colors.Primary.Should().ContainKey( "foreground" );
 
-        theme.Light.Colors.Secondary.Should().HaveCount( 13 );
+        theme.Light.Colors.Secondary.Should().HaveCount( 12 );
         theme.Light.Colors.Secondary.Should().ContainKey( "default" );
         theme.Light.Colors.Secondary.Should().ContainKey( "foreground" );
 
-        theme.Light.Colors.Success.Should().HaveCount( 13 );
+        theme.Light.Colors.Success.Should().HaveCount( 12 );
         theme.Light.Colors.Success.Should().ContainKey( "default" );
         theme.Light.Colors.Success.Should().ContainKey( "foreground" );
 
-        theme.Light.Colors.Warning.Should().HaveCount( 13 );
+        theme.Light.Colors.Warning.Should().HaveCount( 12 );
         theme.Light.Colors.Warning.Should().ContainKey( "default" );
         theme.Light.Colors.Warning.Should().ContainKey( "foreground" );
 
-        theme.Light.Colors.Danger.Should().HaveCount( 13 );
+        theme.Light.Colors.Danger.Should().HaveCount( 12 );
         theme.Light.Colors.Danger.Should().ContainKey( "default" );
         theme.Light.Colors.Danger.Should().ContainKey( "foreground" );
 
-        theme.Light.Colors.Info.Should().HaveCount( 13 );
+        theme.Light.Colors.Info.Should().HaveCount( 12 );
         theme.Light.Colors.Info.Should().ContainKey( "default" );
         theme.Light.Colors.Info.Should().ContainKey( "foreground" );
 
-        theme.Dark.Colors.Default.Should().HaveCount( 13 );
+        theme.Dark.Colors.Default.Should().HaveCount( 12 );
         theme.Dark.Colors.Default.Should().ContainKey( "default" );
         theme.Dark.Colors.Default.Should().ContainKey( "foreground" );
 
-        theme.Dark.Colors.Primary.Should().HaveCount( 13 );
+        theme.Dark.Colors.Primary.Should().HaveCount( 12 );
         theme.Dark.Colors.Primary.Should().ContainKey( "default" );
         theme.Dark.Colors.Primary.Should().ContainKey( "foreground" );
 
-        theme.Dark.Colors.Secondary.Should().HaveCount( 13 );
+        theme.Dark.Colors.Secondary.Should().HaveCount( 12 );
         theme.Dark.Colors.Secondary.Should().ContainKey( "default" );
         theme.Dark.Colors.Secondary.Should().ContainKey( "foreground" );
 
-        theme.Dark.Colors.Success.Should().HaveCount( 13 );
+        theme.Dark.Colors.Success.Should().HaveCount( 12 );
         theme.Dark.Colors.Success.Should().ContainKey( "default" );
         theme.Dark.Colors.Success.Should().ContainKey( "foreground" );
 
-        theme.Dark.Colors.Warning.Should().HaveCount( 13 );
+        theme.Dark.Colors.Warning.Should().HaveCount( 12 );
         theme.Dark.Colors.Warning.Should().ContainKey( "default" );
         theme.Dark.Colors.Warning.Should().ContainKey( "foreground" );
 
-        theme.Dark.Colors.Danger.Should().HaveCount( 13 );
+        theme.Dark.Colors.Danger.Should().HaveCount( 12 );
         theme.Dark.Colors.Danger.Should().ContainKey( "default" );
         theme.Dark.Colors.Danger.Should().ContainKey( "foreground" );
 
-        theme.Dark.Colors.Info.Should().HaveCount( 13 );
+        theme.Dark.Colors.Info.Should().HaveCount( 12 );
         theme.Dark.Colors.Info.Should().ContainKey( "default" );
         theme.Dark.Colors.Info.Should().ContainKey( "foreground" );
     }
