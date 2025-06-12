4ca69e4060594759c7049786c0693cd7c2b4f91a
creanium <john@creanium.net>
2025-06-12 16:03:37 -0600

    Style Improvements for Alert

--- src/LumexUI/Styles/Alert.cs
+++ src/LumexUI/Styles/Alert.cs
@@ -25,38 +25,47 @@ internal static class Alert
 			Slots = new SlotCollection
 			{
 				[nameof( AlertSlots.Base )] = new ElementClass()
-					.Add( "w-full" )
 					.Add( "flex" )
+					.Add( "flex-grow" )
+					.Add( "flex-row" )
+					.Add( "w-full" )
+					.Add( "items-start" )
 					.Add( "py-3" )
 					.Add( "px-4" )
-					.Add( "gap-1" )
-					.Add( "items-start" ),
-
+					.Add( "gap-x-1" ),
+				
 				[nameof( AlertSlots.MainWrapper )] = new ElementClass()
 					.Add( "h-full" )
+					.Add( "flex-grow" )
 					.Add( "min-h-10" )
+					.Add( "ms-2" )
 					.Add( "flex" )
 					.Add( "flex-col" )
-					.Add( "ms-2" )
+					.Add( "box-border" )
 					.Add( "items-start" )
-					.Add( "justify-center" )
-					.Add( "text-inherit" ),
+					.Add( "text-inherit" )
+					.Add( "justify-center" ),
 
 				[nameof( AlertSlots.Title )] = new ElementClass()
+					.Add( "text-small" )
 					.Add( "w-full" )
 					.Add( "font-medium" )
-					.Add( "text-small" ),
-
+					.Add( "block" )
+					.Add( "text-inherit" )
+					.Add( "leading-5" ),
+				
 				[nameof( AlertSlots.Description )] = new ElementClass()
-					.Add( "w-full" )
-					.Add( "text-small" ),
+					.Add( "pl-[1px]" )
+					.Add( "text-small" )
+					.Add( "font-normal" )
+					.Add( "text-inherit" ),
 
 				[nameof( AlertSlots.CloseButton )] = new ElementClass()
 					.Add( "relative" )
 					.Add( "text-inherit" )
 					.Add( "translate-x-1" )
-					.Add( "text-inherit" ),
-
+					.Add( "-translate-y-1" ),
+				
 				[nameof( AlertSlots.IconWrapper )] = new ElementClass()
 					.Add( "flex-none" )
 					.Add( "relative" )
@@ -83,8 +92,7 @@ internal static class Alert
 					[nameof( AlertVariant.Solid )] = new SlotCollection(),
 					[nameof( AlertVariant.Outlined )] = new SlotCollection
 					{
-						[nameof( AlertSlots.Base )] = "border bg-transparent",
-						[nameof( AlertSlots.IconWrapper )] = "shadow-small"
+						[nameof( AlertSlots.Base )] = "border bg-transparent"
 					},
 					[nameof( AlertVariant.Flat )] = new SlotCollection
 					{
@@ -92,8 +100,7 @@ internal static class Alert
 					},
 					[nameof( AlertVariant.Faded )] = new SlotCollection
 					{
-						[nameof( AlertSlots.Base )] = "border",
-						[nameof( AlertSlots.IconWrapper )] = "shadow-small border-1"
+						[nameof( AlertSlots.Base )] = "border"
 					}
 				},
 
@@ -128,12 +135,20 @@ internal static class Alert
 						[nameof( AlertSlots.IconWrapper )] = "hidden"
 					}
 				},
+				
+				[nameof( LumexAlert.HideIconWrapper )] = new VariantValueCollection
+				{
+					[bool.TrueString] = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = "gap-x-0",
+						[nameof( AlertSlots.IconWrapper )] = "!bg-transparent !shadow-none !border-none"
+					}
+				}
 			},
 
 			CompoundVariants =
 			[
 				// solid & color
-				
 				new CompoundVariant
 				{
 					Conditions = new()
@@ -336,6 +351,121 @@ internal static class Alert
 						[nameof( AlertSlots.IconWrapper )] = "bg-info-50 dark:bg-info-100 border-info-100",
 					}
 				},
+				
+				// faded & color
+				new CompoundVariant
+				{
+					Conditions = new()
+					{
+						[nameof( LumexAlert.Variant )] = nameof( AlertVariant.Faded ),
+						[nameof( LumexAlert.Color )] = nameof( ThemeColor.Default )
+					},
+					Classes = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = new ElementClass()
+							.Add( "border-default-300 dark:border-default-200" )
+							.Add( "bg-default-100 dark:bg-default-50/50"),
+						[nameof( AlertSlots.Description )] = "text-default-600",
+						[nameof( AlertSlots.CloseButton )] = "text-default-400",
+						[nameof( AlertSlots.IconWrapper )] = "bg-default-50 dark:bg-default-100 border-default-200",
+					}
+				},
+				new CompoundVariant
+				{
+					Conditions = new()
+					{
+						[nameof( LumexAlert.Variant )] = nameof( AlertVariant.Faded ),
+						[nameof( LumexAlert.Color )] = nameof( ThemeColor.Primary )
+					},
+					Classes = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = new ElementClass()
+							.Add( "border-primary-200 dark:border-primary-100" )
+							.Add( "bg-primary-50 dark:bg-primary-50/50" ),
+						[nameof( AlertSlots.CloseButton )] = "text-primary-500 hover:bg-primary-200",
+						[nameof( AlertSlots.IconWrapper )] = "bg-primary-50 dark:bg-primary-100 border-primary-100",
+					}
+				},
+				new CompoundVariant
+				{
+					Conditions = new()
+					{
+						[nameof( LumexAlert.Variant )] = nameof( AlertVariant.Faded ),
+						[nameof( LumexAlert.Color )] = nameof( ThemeColor.Secondary )
+					},
+					Classes = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = new ElementClass()
+							.Add( "border-secondary-200" )
+							.Add( "bg-secondary-50 dark:bg-secondary-50/50" ),
+						[nameof( AlertSlots.CloseButton )] = "text-secondary-500 hover:bg-secondary-200",
+						[nameof( AlertSlots.IconWrapper )] = "bg-secondary-50 dark:bg-secondary-100 border-secondary-100",
+					}
+				},
+				new CompoundVariant
+				{
+					Conditions = new()
+					{
+						[nameof( LumexAlert.Variant )] = nameof( AlertVariant.Faded ),
+						[nameof( LumexAlert.Color )] = nameof( ThemeColor.Success )
+					},
+					Classes = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = new ElementClass()
+							.Add( "border-success-300 dark:border-success-100" )
+							.Add( "bg-success-50 dark:bg-success-50/50" ),
+						[nameof( AlertSlots.CloseButton )] = "text-success-500 hover:bg-success-200",
+						[nameof( AlertSlots.IconWrapper )] = "bg-success-50 dark:bg-success-100 border-success-100",
+					}
+				},
+				new CompoundVariant
+				{
+					Conditions = new()
+					{
+						[nameof( LumexAlert.Variant )] = nameof( AlertVariant.Faded ),
+						[nameof( LumexAlert.Color )] = nameof( ThemeColor.Warning )
+					},
+					Classes = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = new ElementClass()
+							.Add( "border-warning-300 dark:border-warning-100" )
+							.Add( "bg-warning-50 dark:bg-warning-50/50" ),
+						[nameof( AlertSlots.CloseButton )] = "text-warning-500 hover:bg-warning-200",
+						[nameof( AlertSlots.IconWrapper )] = "bg-warning-50 dark:bg-warning-100 border-warning-100",
+					}
+				},
+				new CompoundVariant
+				{
+					Conditions = new()
+					{
+						[nameof( LumexAlert.Variant )] = nameof( AlertVariant.Faded ),
+						[nameof( LumexAlert.Color )] = nameof( ThemeColor.Danger )
+					},
+					Classes = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = new ElementClass()
+							.Add( "border-danger-200 dark:border-danger-100" )
+							.Add( "bg-danger-50 dark:bg-danger-50/50" ),
+						[nameof( AlertSlots.CloseButton )] = "text-danger-500 hover:bg-danger-200",
+						[nameof( AlertSlots.IconWrapper )] = "bg-danger-50 dark:bg-danger-100 border-danger-100",
+					}
+				},
+				new CompoundVariant
+				{
+					Conditions = new()
+					{
+						[nameof( LumexAlert.Variant )] = nameof( AlertVariant.Faded ),
+						[nameof( LumexAlert.Color )] = nameof( ThemeColor.Info )
+					},
+					Classes = new SlotCollection
+					{
+						[nameof( AlertSlots.Base )] = new ElementClass()
+							.Add( "border-info-200 dark:border-info-100" )
+							.Add( "bg-info-50 dark:bg-info-50/50" ),
+						[nameof( AlertSlots.CloseButton )] = "text-info-500 hover:bg-info-200",
+						[nameof( AlertSlots.IconWrapper )] = "bg-info-50 dark:bg-info-100 border-info-100",
+					}
+				}
 			]
 		} );
 	}
--- src/LumexUI/Styles/ColorVariants.cs
+++ src/LumexUI/Styles/ColorVariants.cs
@@ -4,75 +4,86 @@ namespace LumexUI.Styles;
 
 internal class ColorVariants
 {
-    public readonly static Dictionary<ThemeColor, string> Solid = new()
-    {
-        [ThemeColor.None] = "",
-        [ThemeColor.Default] = "bg-default text-default-foreground",
-        [ThemeColor.Primary] = "bg-primary text-primary-foreground",
-        [ThemeColor.Secondary] = "bg-secondary text-secondary-foreground",
-        [ThemeColor.Success] = "bg-success text-success-foreground",
-        [ThemeColor.Warning] = "bg-warning text-warning-foreground",
-        [ThemeColor.Danger] = "bg-danger text-danger-foreground",
-        [ThemeColor.Info] = "bg-info text-info-foreground"
-    };
+	public readonly static Dictionary<ThemeColor, string> Solid = new()
+	{
+		[ThemeColor.None] = "",
+		[ThemeColor.Default] = "bg-default text-default-foreground",
+		[ThemeColor.Primary] = "bg-primary text-primary-foreground",
+		[ThemeColor.Secondary] = "bg-secondary text-secondary-foreground",
+		[ThemeColor.Success] = "bg-success text-success-foreground",
+		[ThemeColor.Warning] = "bg-warning text-warning-foreground",
+		[ThemeColor.Danger] = "bg-danger text-danger-foreground",
+		[ThemeColor.Info] = "bg-info text-info-foreground"
+	};
 
-    public readonly static Dictionary<ThemeColor, string> Outlined = new()
-    {
-        [ThemeColor.None] = "",
-        [ThemeColor.Default] = "bg-transparent border-default text-foreground",
-        [ThemeColor.Primary] = "bg-transparent border-primary text-primary",
-        [ThemeColor.Secondary] = "bg-transparent border-secondary text-secondary",
-        [ThemeColor.Success] = "bg-transparent border-success text-success",
-        [ThemeColor.Warning] = "bg-transparent border-warning text-warning",
-        [ThemeColor.Danger] = "bg-transparent border-danger text-danger",
-        [ThemeColor.Info] = "bg-transparent border-info text-info"
-    };
+	public readonly static Dictionary<ThemeColor, string> Outlined = new()
+	{
+		[ThemeColor.None] = "",
+		[ThemeColor.Default] = "bg-transparent border-default text-foreground",
+		[ThemeColor.Primary] = "bg-transparent border-primary text-primary",
+		[ThemeColor.Secondary] = "bg-transparent border-secondary text-secondary",
+		[ThemeColor.Success] = "bg-transparent border-success text-success",
+		[ThemeColor.Warning] = "bg-transparent border-warning text-warning",
+		[ThemeColor.Danger] = "bg-transparent border-danger text-danger",
+		[ThemeColor.Info] = "bg-transparent border-info text-info"
+	};
 
-    public readonly static Dictionary<ThemeColor, string> Flat = new()
-    {
-        [ThemeColor.None] = "",
-        [ThemeColor.Default] = "bg-default/40 text-default-700",
-        [ThemeColor.Primary] = "bg-primary-100 text-primary-700",
-        [ThemeColor.Secondary] = "bg-secondary-100 text-secondary-700",
-        [ThemeColor.Success] = "bg-success-100 text-success-700",
-        [ThemeColor.Warning] = "bg-warning-100 text-warning-700",
-        [ThemeColor.Danger] = "bg-danger-100 text-danger-700",
-        [ThemeColor.Info] = "bg-info-100 text-info-700"
-    };
+	public readonly static Dictionary<ThemeColor, string> Flat = new()
+	{
+		[ThemeColor.None] = "",
+		[ThemeColor.Default] = "bg-default/40 text-default-700",
+		[ThemeColor.Primary] = "bg-primary-100 text-primary-700",
+		[ThemeColor.Secondary] = "bg-secondary-100 text-secondary-700",
+		[ThemeColor.Success] = "bg-success-100 text-success-700",
+		[ThemeColor.Warning] = "bg-warning-100 text-warning-700",
+		[ThemeColor.Danger] = "bg-danger-100 text-danger-700",
+		[ThemeColor.Info] = "bg-info-100 text-info-700"
+	};
 
-    public readonly static Dictionary<ThemeColor, string> Shadow = new()
-    {
-        [ThemeColor.None] = "",
-        [ThemeColor.Default] = "shadow-md shadow-default/40 bg-default text-default-foreground",
-        [ThemeColor.Primary] = "shadow-md shadow-primary/40 bg-primary text-primary-foreground",
-        [ThemeColor.Secondary] = "shadow-md shadow-secondary/40 bg-secondary text-secondary-foreground",
-        [ThemeColor.Success] = "shadow-md shadow-success/40 bg-success text-success-foreground",
-        [ThemeColor.Warning] = "shadow-md shadow-warning/40 bg-warning text-warning-foreground",
-        [ThemeColor.Danger] = "shadow-md shadow-danger/40 bg-danger text-danger-foreground",
-        [ThemeColor.Info] = "shadow-md shadow-info/40 bg-info text-info-foreground"
-    };
+	public readonly static Dictionary<ThemeColor, string> Shadow = new()
+	{
+		[ThemeColor.None] = "",
+		[ThemeColor.Default] = "shadow-md shadow-default/40 bg-default text-default-foreground",
+		[ThemeColor.Primary] = "shadow-md shadow-primary/40 bg-primary text-primary-foreground",
+		[ThemeColor.Secondary] = "shadow-md shadow-secondary/40 bg-secondary text-secondary-foreground",
+		[ThemeColor.Success] = "shadow-md shadow-success/40 bg-success text-success-foreground",
+		[ThemeColor.Warning] = "shadow-md shadow-warning/40 bg-warning text-warning-foreground",
+		[ThemeColor.Danger] = "shadow-md shadow-danger/40 bg-danger text-danger-foreground",
+		[ThemeColor.Info] = "shadow-md shadow-info/40 bg-info text-info-foreground"
+	};
 
-    public readonly static Dictionary<ThemeColor, string> Ghost = new()
-    {
-        [ThemeColor.None] = "",
-        [ThemeColor.Default] = "borde-default text-default-foreground",
-        [ThemeColor.Primary] = "border-primary text-primary",
-        [ThemeColor.Secondary] = "border-secondary text-secondary",
-        [ThemeColor.Success] = "border-success text-success",
-        [ThemeColor.Warning] = "border-warning text-warning",
-        [ThemeColor.Danger] = "border-danger text-danger",
-        [ThemeColor.Info] = "border-info text-info"
-    };
+	public readonly static Dictionary<ThemeColor, string> Ghost = new()
+	{
+		[ThemeColor.None] = "",
+		[ThemeColor.Default] = "border-default text-default-foreground",
+		[ThemeColor.Primary] = "border-primary text-primary",
+		[ThemeColor.Secondary] = "border-secondary text-secondary",
+		[ThemeColor.Success] = "border-success text-success",
+		[ThemeColor.Warning] = "border-warning text-warning",
+		[ThemeColor.Danger] = "border-danger text-danger",
+		[ThemeColor.Info] = "border-info text-info"
+	};
 
-    public readonly static Dictionary<ThemeColor, string> Light = new()
-    {
-        [ThemeColor.None] = "",
-        [ThemeColor.Default] = "bg-transparent text-default-foreground",
-        [ThemeColor.Primary] = "bg-transparent text-primary",
-        [ThemeColor.Secondary] = "bg-transparent text-secondary",
-        [ThemeColor.Success] = "bg-transparent text-success",
-        [ThemeColor.Warning] = "bg-transparent text-warning",
-        [ThemeColor.Danger] = "bg-transparenttext-danger",
-        [ThemeColor.Info] = "bg-transparent text-info"
-    };
+	public readonly static Dictionary<ThemeColor, string> Faded = new()
+	{
+		[ThemeColor.Default] = "border-default bg-default-100 text-default-foreground",
+		[ThemeColor.Primary] = "border-default bg-default-100 text-primary",
+		[ThemeColor.Secondary] = "border-default bg-default-100 text-secondary",
+		[ThemeColor.Success] = "border-default bg-default-100 text-success",
+		[ThemeColor.Warning] = "border-default bg-default-100 text-warning",
+		[ThemeColor.Danger] = "border-default bg-default-100 text-danger",
+		[ThemeColor.Info] = "border-default bg-default-100 text-info",
+	};
+
+	public readonly static Dictionary<ThemeColor, string> Light = new()
+	{
+		[ThemeColor.None] = "",
+		[ThemeColor.Default] = "bg-transparent text-default-foreground",
+		[ThemeColor.Primary] = "bg-transparent text-primary",
+		[ThemeColor.Secondary] = "bg-transparent text-secondary",
+		[ThemeColor.Success] = "bg-transparent text-success",
+		[ThemeColor.Warning] = "bg-transparent text-warning",
+		[ThemeColor.Danger] = "bg-transparent text-danger",
+		[ThemeColor.Info] = "bg-transparent text-info"
+	};
 }
